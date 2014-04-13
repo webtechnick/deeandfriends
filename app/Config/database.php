1d1b4cacd5c5f09a37c3f8c59bfc1fea6e955a81
Nick Baker <nick@webtechnick.com>
2014-04-12 20:42:05 -0600

    Skeleton App

---
+++ app/Config/database.php
@@ -0,0 +1,12 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => '127.0.0.1',
+		'login' => 'webtechn_daf',
+		'password' => 'rEsBpVwmw3UDf7nB',
+		'database' => 'webtechn_daf',
+	);
+}
